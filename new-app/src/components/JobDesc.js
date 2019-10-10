67ad153aef5931fef996fb6cf6672290528e7c43
ofega <fegaeze@gmail.com>
2019-10-10 16:38:23 +0100

    set up project structure with dependencies

---
+++ new-app/src/components/JobDesc.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const JobDesc = () => {
+  return (
+    <h1></h1>
+  );
+}
+
+export default JobDesc;
---
+++ new-app/src/components/QueryForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const QueryForm = () => {
+  return (
+    <h1>QueryForm</h1>
+  );
+}
+
+export default QueryForm;
